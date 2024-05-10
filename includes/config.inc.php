c1cd7201e35883310aac590aca4dfa83572d4be8
AnnoyingMaster <annoyingpresents@gmail.com>
2024-05-10 22:21:20 +0200

    Galéria
    Elkészült a galéria fül

--- includes/config.inc.php
+++ includes/config.inc.php
@@ -1,4 +1,11 @@
 <?php
+
+$MAPPA = './images/memes/';
+$TIPUSOK = array ('.jpg', '.png');
+$MEDIATIPUSOK = array('image/jpeg', 'image/png');
+$DATUMFORMA = "Y.m.d. H:i";
+$MAXMERET = 500*1024;
+
 $ablakcim = array(
     'cim' => 'Skill Issue',
 );
@@ -21,8 +28,10 @@ $oldalak = array(
     'belepes' => array('fajl' => 'belepes', 'szoveg' => 'Belépés', 'menun' => array(1,0)),
     'kilepes' => array('fajl' => 'kilepes', 'szoveg' => 'Kilépés', 'menun' => array(0,1)),
     'belep' => array('fajl' => 'belep', 'szoveg' => '', 'menun' => array(0,0)),
+    'upload' => array('fajl' => 'upload', 'szoveg' => '', 'menun' => array(0,0)),
     'regisztral' => array('fajl' => 'regisztral', 'szoveg' => '', 'menun' => array(0,0))
 );
 
 $hiba_oldal = array ('fajl' => '404', 'szoveg' => 'A keresett oldal nem található!');
+
 ?>
