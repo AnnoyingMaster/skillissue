637a51b60c330d4b2bdbf4caf5cd1cd666b80775
dacibarta <bartadavid327@gmail.com>
2024-05-11 18:54:06 +0200

    Tesztek

--- templates/pages/tesztek.tpl.php
+++ templates/pages/tesztek.tpl.php
@@ -1,33 +1,9 @@
-<table>
-    <caption>5. gyakorlat 1. feladat:<br>TÁBLÁZAT</caption>
-    <tr>
-        <th>Employee</th>
-        <th>Salary</th>
-        <th>Bonus</th>
-        <th>Supervisor</th>
-    </tr>
-    <tr>
-        <td>Stephen C. Cox</td>
-        <td>$300</td>
-        <td>$50</td>
-        <td>Bob</td>
-    </tr>
-    <tr>
-        <td>Josephin Tan</td>
-        <td>$150</td>
-        <td>-</td>
-        <td>Annie</td>
-    </tr>
-    <tr>
-        <td>Joyce Ming</td>
-        <td>$200</td>
-        <td>$35</td>
-        <td>Andy</td>
-    </tr>
-    <tr>
-        <td>James A. Pentel</td>
-        <td>$175</td>
-        <td>$25</td>
-        <td>Annie</td>
-    </tr>
-</table>
+<form action="<?= ($url == '/') ? '.' : ('?oldal=matek') ?>" method="post">
+    <label id="teszt" for="teszt1">Matematika teszt</label>
+    <input id="teszt1" class="tesztek" type="submit" value="Matematika teszt" />
+</form>
+<br></br>
+<form action="<?= ($url == '/') ? '.' : ('?oldal=angol') ?>" method="post">
+    <label id="teszt" for="teszt2">Angol teszt</label>
+    <input id="teszt2" class="tesztek" type="submit" value="Angol teszt" />
+</form>
