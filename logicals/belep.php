0fe34f90707c3cc9d8b1321c9655352e99f77486
AnnoyingMaster <annoyingpresents@gmail.com>
2024-05-11 14:41:19 +0200

    Adatbázis átalakítva
    Átalakítottam az adatbázist, elvégeztem a módosításokat a belépés és regisztráció logicalokban, hogy működjenek

--- logicals/belep.php
+++ logicals/belep.php
@@ -2,14 +2,14 @@
 if(isset($_POST['felhasznalo']) && isset($_POST['jelszo'])) {
     try {
         // Kapcsolódás
-        $dbh = new PDO('mysql:host=localhost;dbname=gyakorlat7', 'root', '',
+        $dbh = new PDO('mysql:host=localhost;dbname=skillissue', 'root', '',
                         array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
         // Felhsználó keresése
-        $sqlSelect = "select id, csaladi_nev, uto_nev from felhasznalok where bejelentkezes = :bejelentkezes and jelszo = sha1(:jelszo)";
+        $sqlSelect = "select id, csaladi_nev, uto_nev from felhasznalok where felhasznalo = :felhasznalo and jelszo = sha1(:jelszo)";
         $sth = $dbh->prepare($sqlSelect);
-        $sth->execute(array(':bejelentkezes' => $_POST['felhasznalo'], ':jelszo' => $_POST['jelszo']));
+        $sth->execute(array(':felhasznalo' => $_POST['felhasznalo'], ':jelszo' => $_POST['jelszo']));
         $row = $sth->fetch(PDO::FETCH_ASSOC);
         if($row) {
             $_SESSION['csn'] = $row['csaladi_nev']; $_SESSION['un'] = $row['uto_nev']; $_SESSION['login'] = $_POST['felhasznalo'];
--- logicals/regisztral.php
+++ logicals/regisztral.php
@@ -2,25 +2,25 @@
 if(isset($_POST['felhasznalo']) && isset($_POST['jelszo']) && isset($_POST['vezeteknev']) && isset($_POST['utonev'])) {
     try {
         // Kapcsolódás
-        $dbh = new PDO('mysql:host=localhost;dbname=gyakorlat7', 'root', '',
+        $dbh = new PDO('mysql:host=localhost;dbname=skillissue', 'root', '',
                         array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
         // Létezik már a felhasználói név?
-        $sqlSelect = "select id from felhasznalok where bejelentkezes = :bejelentkezes";
+        $sqlSelect = "select id from felhasznalok where felhasznalo = :felhasznalo";
         $sth = $dbh->prepare($sqlSelect);
-        $sth->execute(array(':bejelentkezes' => $_POST['felhasznalo']));
+        $sth->execute(array(':felhasznalo' => $_POST['felhasznalo']));
         if($row = $sth->fetch(PDO::FETCH_ASSOC)) {
             $uzenet = "A felhasználói név már foglalt!";
             $ujra = "true";
         }
         else {
             // Ha nem létezik, akkor regisztráljuk
-            $sqlInsert = "insert into felhasznalok(id, csaladi_nev, uto_nev, bejelentkezes, jelszo)
-                          values(0, :csaladinev, :utonev, :bejelentkezes, :jelszo)";
+            $sqlInsert = "insert into felhasznalok(id, csaladi_nev, uto_nev, felhasznalo, jelszo)
+                          values(0, :csaladinev, :utonev, :felhasznalo, :jelszo)";
             $stmt = $dbh->prepare($sqlInsert); 
             $stmt->execute(array(':csaladinev' => $_POST['vezeteknev'], ':utonev' => $_POST['utonev'],
-                                 ':bejelentkezes' => $_POST['felhasznalo'], ':jelszo' => sha1($_POST['jelszo']))); 
+                                 ':felhasznalo' => $_POST['felhasznalo'], ':jelszo' => sha1($_POST['jelszo'])));
             if($count = $stmt->rowCount()) {
                 $newid = $dbh->lastInsertId();
                 $uzenet = "A regisztrációja sikeres.<br>Azonosítója: {$newid}";                     
